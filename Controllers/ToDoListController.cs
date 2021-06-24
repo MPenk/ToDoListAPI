4c730fdf7a618d71fa1fddfcd5f7487aabb58762
Michał <michu.penk@gmail.com>
2021-06-25 00:32:51 +0200

    Dodaj pliki projektów.

---
+++ Controllers/ToDoListController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace ToDoList.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ToDoListController : ControllerBase
+    {
+
+        // POST <ToDoListController>/{id}
+        [HttpPost("{id}")]
+        public string PostShowById(int id)
+        {
+            try
+            {
+                return ToDoListItem.GetTask(id).TaskName;
+            }
+            catch (Exception)
+            {
+
+                return "BRAK";
+            }
+        }
+
+        // POST <ToDoListController>/all
+        [HttpPost("all")]
+        public IEnumerable<ToDoListItem> PostShowAll()
+        {
+            return ToDoListItem.GetTasks();
+        }
+
+        // GET <ToDoListController>/all
+        [HttpGet("all")]
+        public IEnumerable<ToDoListItem> GetShowAll()
+        {
+            return ToDoListItem.GetTasks();
+        }
+
+        // POST <ToDoListController>/all?taskName={taskName}
+        [HttpPost("add")]
+        public int PostAddNew(string taskName)
+        {
+            try
+            {
+                ToDoListItem.AddTask(taskName);
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+            return 1;
+        }
+
+        // PUT <ToDoListController>/all?taskName={taskName}
+        [HttpPut("add")]
+        public int PUTAddNew(string taskName)
+        {
+            try
+            {
+                ToDoListItem.AddTask(taskName);
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+            return 1;
+        }
+
+        // POST <ToDoListController>/{id}/remove
+        [HttpPost("{id}/remove")]
+        public int PostRemoveById(int id)
+        {
+            try
+            {
+                ToDoListItem.RemoveTask(id);
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+            return 1;
+        }
+
+        // DELETE <ToDoListController>/{id}/remove
+        [HttpDelete ("{id}/remove")]
+        public int DeleteRemoveById(int id)
+        {
+            try
+            {
+                ToDoListItem.RemoveTask(id);
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+            return 1;
+        }
+    }
+}
---
+++ ToDoListItem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ToDoList
+{
+    public class ToDoListItem
+    {
+        public static List<ToDoListItem> toDoItems = new List<ToDoListItem>();
+
+        public string TaskName { get; set; }
+
+        public ToDoListItem(string taskName)
+        {
+            this.TaskName = taskName;
+        }
+        static public void RemoveTask(int id)
+        {
+            if (id > toDoItems.Count - 1 || id < 0 || toDoItems.Count==0)
+                throw new ArgumentOutOfRangeException("Wrong ID!");
+            toDoItems.RemoveAt(id);
+        }
+
+        static public void AddTask(string taskName)
+        {
+            toDoItems.Add(new ToDoListItem(taskName));
+        }
+
+        static public List<ToDoListItem> GetTasks()
+        {
+            return toDoItems;
+        }
+
+        static public ToDoListItem GetTask(int id)
+        {
+            if (id > toDoItems.Count - 1 || id < 0 || toDoItems.Count == 0)
+                throw new ArgumentOutOfRangeException("Wrong ID!");
+            return toDoItems[id];
+        }
+    }
+}
